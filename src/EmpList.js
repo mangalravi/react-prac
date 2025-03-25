f937703f2f7291946a265633dbb356d563c1b541
Ravi Mangal <ravimangal2884@gmail.com>
2025-03-25 18:38:44 +0530

    redux + slider + model outside click

--- src/EmpList.js
+++ src/EmpList.js
@@ -1,22 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const EmpList = () => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch('https://dummy.restapiexample.com/api/v1/employees');
-      .then(res => res.json())
-      .then(data => setData(data.data)) 
-      .catch(error => console.error('Error fetching data:', error)); 
+    fetch("https://dummy.restapiexample.com/api/v1/employees")
+      .then((res) => res.json())
+      .then((data) => setData(data.data))
+      .catch((error) => console.error("Error fetching data:", error));
   }, []);
 
   return (
     <ul>
-      {
-         data.map(item => (
-          <li key={item.id}>{item.employee_name}</li>
-        ))
-      }
+      {data.map((item) => (
+        <li key={item.id}>{item.employee_name}</li>
+      ))}
     </ul>
   );
 };
